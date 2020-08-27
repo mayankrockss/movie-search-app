2e6f1bf0efaeee5d43b7e5b9e6f6eef9b655dd00
Mayank Singh <msingh.mayank@gmail.com>
2020-08-27 20:08:45 +0530

    Updates

---
+++ precache-manifest.2a3ceea55742e502588fc46370b356e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868de0e3cdd5d205a6f3e10fdda208dd",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "f888abfe224b3ca3a8b7",
+    "url": "/movie-search-app/static/css/main.d884eb41.chunk.css"
+  },
+  {
+    "revision": "110ae3c7a7cc97394490",
+    "url": "/movie-search-app/static/js/2.e2f238c4.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/movie-search-app/static/js/2.e2f238c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f888abfe224b3ca3a8b7",
+    "url": "/movie-search-app/static/js/main.a89abc40.chunk.js"
+  },
+  {
+    "revision": "93854698e18f7c10401e",
+    "url": "/movie-search-app/static/js/runtime-main.fb6de319.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/movie-search-app/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/movie-search-app/static/media/spinner.235b9b3e.gif"
+  }
+]);
